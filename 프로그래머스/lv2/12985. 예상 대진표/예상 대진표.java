7fb6647fa202d7d9a7f174800f23cc5fb5013525
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2023-02-02 08:20:03 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(a!=b){
+            answer++;
+            a=++a/2;
+            b=++b/2;
+        }
+        return answer;
+    }
+}
