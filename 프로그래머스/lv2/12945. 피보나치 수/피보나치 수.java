cce2565917968a82e836c74aece7c57bac7e4d4b
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2023-02-02 08:59:11 +0900

    [level 2] Title: 피보나치 수, Time: 2.28 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int[] arr = new int[n+1];
+        arr[0]=0; arr[1]=1;
+        for(int a=2;a<=n;a++){
+            arr[a]=(arr[a-1]+arr[a-2])%1234567;
+        }
+        return arr[n]; 
+    }
+}
