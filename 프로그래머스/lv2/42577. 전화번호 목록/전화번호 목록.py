cbd31f55577f3358dff23d8c82310a1ddb867101
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2023-05-24 16:41:52 +0900

    [level 2] Title: 전화번호 목록, Time: 90.92 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,5 @@
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(0, len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]): return False
+    return True #접두사인 것 없음
