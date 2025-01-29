6367be2398f05f290fbec42a0234b6d3d8c02d2f
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-01-30 00:08:06 +0900

    [Silver IV] Title: 동전 0, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<n;i++){
+            stack.push(Integer.parseInt(br.readLine()));
+        }
+        int coinCnt = 0;
+        while(k>0){
+            int maxCoin = stack.pop();
+            coinCnt+=k/maxCoin;
+            k%=maxCoin;
+        }
+        System.out.print(coinCnt);
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
