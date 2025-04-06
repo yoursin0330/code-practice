88ce275d320a126e1bec1422bd501878a18f5e0d
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-04-06 12:38:00 +0900

    [Bronze V] Title: Hello World, Time: 92 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,8 @@
+public class Main{
+    public void solution() throws Exception{
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
