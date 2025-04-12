85e1563096ab5315224d9eb61f6d01b60d51a631
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-04-12 23:39:10 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public void solution() throws Exception {
+        System.out.println("         ,r'\"7\nr`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
