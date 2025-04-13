7550946db48b14cd2bb2f129798a9b6dad753919
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-04-13 14:36:33 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,14 @@
+public class Main {
+    public void solution() throws Exception {
+
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
