d7c4415bc791492ab7f7551e5f1ce5c6050d9dba
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-04-13 14:36:55 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+public class Main {
+    public void solution() throws Exception {
+
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
