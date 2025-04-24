6c2fde4784d3a65e192305149a9fc5de4865f227
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-04-24 21:24:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 0.09 ms
+메모리: 9.16 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 23일 19:21:16
+2025년 04월 24일 21:24:03
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,19 +1,21 @@
 from collections import defaultdict
 def solution(genres, plays):
     answer = []
-    genre_play_cnt = defaultdict(int)
+    n = len(genres)
+    genre_play_count = defaultdict(int)
     genre_songs = dict()
-    for i in range(len(genres)):
-        genre_play_cnt[genres[i]]+=plays[i]
-        if genres[i] in genre_songs: genre_songs[genres[i]].append((plays[i],i))
-        else: genre_songs[genres[i]] = [(plays[i], i)]
-    sorted_genres = sorted(genre_play_cnt, key = lambda x: genre_play_cnt[x], reverse=True)
-    for genre in sorted_genres:
-        song_list = genre_songs[genre]
-        song_list.sort(key=lambda x: (-x[0],x[1]))
-        cnt=0
-        for x in song_list:
-            if cnt==2: break
-            answer.append(x[1])
-            cnt+=1
+    for i in range(n):
+        genre_play_count[genres[i]]+=plays[i] # 장르당 총 재생 횟수
+        if genres[i] in genre_songs: # 장르별 각 노래 [인덱스, 재생수] 저장
+            genre_songs[genres[i]].append((i, plays[i]))
+        else: genre_songs[genres[i]] = [(i, plays[i])]
+        
+    genre_sorted = sorted(genre_play_count.items(), key=lambda x : x[1], reverse=True) # 재생순 내림차순 정렬
+    for genre,_ in genre_sorted:
+        songlist = sorted(genre_songs[genre], key=lambda x : x[1], reverse=True)
+        idx=0
+        for song in songlist:
+            if idx>1: break
+            answer.append(song[0])
+            idx+=1
     return answer
