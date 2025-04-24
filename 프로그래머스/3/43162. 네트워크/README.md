8430830c4e2190b717b0a791389d7a845d7dd4f4
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-04-24 16:32:38 +0900

    [level 3] Title: 네트워크, Time: 0.79 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.52 ms
+메모리: 9.45 MB, 시간: 0.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 24일 23:35:32
+2025년 04월 24일 16:32:36
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,19 @@
+from collections import deque
 def solution(n, computers):
+    cnt=0
     visited = [False]*n
-    def dfs(node):
-        visited[node] = True
-        for connect in range(n):
-            if computers[node][connect] ==1 and not visited[connect]:
-                dfs(connect)
-    answer = 0
-    
+    queue = deque()
     for i in range(n):
-        if not visited[i]:
-            answer+=1
-            dfs(i)
-    return answer
+        if visited[i]: continue
+        visited[i]=True
+        cnt+=1
+        for j in range(i+1, n):
+            if computers[i][j]==1 and not visited[j]:
+                dfs(j, n,computers, visited)
+    return cnt
+def dfs(now, n, computers, visited):
+    visited[now]=True
+    for idx in range(n):
+        if computers[now][idx]==1 and not visited[idx]:
+            dfs(idx, n, computers,visited)
+    return
