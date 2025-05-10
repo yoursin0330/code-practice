c95a185f8c1dfa4a86fdb64fdee0e9b4b1213c34
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-05-10 12:12:04 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -6,9 +6,8 @@ dp=[[0,0] for _ in range(n+1)]
 answer=0
 for i in range(1,n+1):
     dp[i] = list(map(int,input().split())) # t, p
-    maxsum=0
-    for x in range(i):
-        if(dp[x][0]<=i) : maxsum = max(maxsum, dp[x][1])
+    maxsum= max((x[1] for x in dp[:i] if x[0]<=i), default=0)
+
     dp[i][1] += maxsum
     dp[i][0] += i
 
