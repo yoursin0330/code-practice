b0f3a47a13843c46c7ea2620e72fe0e628be0a84
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-08-31 17:47:51 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n,k = map(int, input().split())
+cnt=0
+num=0
+for i in range(1, n+1):
+    if n%i==0: 
+        cnt+=1
+        if cnt==k:
+            num=i
+            break
+print(num)
