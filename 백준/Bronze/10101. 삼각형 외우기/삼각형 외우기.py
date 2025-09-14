39b5aa647a656b59dc8690f2be4dda81f725aa9e
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-09-14 19:10:29 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -5,6 +5,6 @@ for _ in range(3):
     arr.append(int(input()))
 arr.sort()
 if sum(arr)!=180: print("Error")
-elif arr[0]==arr[1]==arr[2]: print("Equilateral")
-elif arr[0]!=arr[1] and arr[1]!=arr[2] and arr[0]!=arr[2]: print("Scalene")
+elif arr[0]==arr[2]: print("Equilateral")
+elif arr[0]<arr[1]<arr[2]: print("Scalene")
 else: print("Isosceles")
