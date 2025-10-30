4a59c27105264882385d64dfc863ca0121883e25
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2025-10-30 14:22:57 +0900

    [Silver I] Title: 그림, Time: 420 ms, Memory: 96524 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -8,18 +8,17 @@ for _ in range(n):
 mv=[[-1,0],[0,-1],[1,0],[0,1]]
 picture=[]
 def dfs(y,x,idx):
+    graph[y][x]=-1
+    picture[idx]+=1
     for dy, dx in mv:
         ny,nx = dy+y, dx+x
         if 0<=ny<n and 0<=nx<m and graph[ny][nx]==1:
-            graph[ny][nx]=-1
-            picture[idx]+=1
             dfs(ny,nx,idx)
 idx=0
 for y in range(n):
     for x in range(m):
         if graph[y][x]==1:
-            picture.append(1)
-            graph[y][x]=-1
+            picture.append(0)
             dfs(y,x,idx)
             idx+=1
 print(idx)
